249c85f32865a3a3f87b93fffbc1c13e4b9d75c4
ezekielkouassi <121268728+ezekielKouassi-dev@users.noreply.github.com>
2023-03-11 10:08:03 +0000

    first commit

---
+++ src/main/java/di/learning/clean/ma/ci/CleanMaCiApplication.java
@@ -0,0 +1,13 @@
+package di.learning.clean.ma.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanMaCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanMaCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/di/learning/clean/ma/ci/CleanMaCiApplicationTests.java
@@ -0,0 +1,13 @@
+package di.learning.clean.ma.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanMaCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
